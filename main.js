e827c93c761aa0639c0a7f5fea435b788344a367
Paul LeMarquand <plemarquand@qnx.com>
2015-02-28 01:41:56 -0500

    Using .env files for config

--- main.js
+++ main.js
@@ -4,23 +4,26 @@ var anchors = require('./lib/newsanchor');
 var debouncer = require('./lib/debouncer');
 var validator = require('./lib/timecop');
 
-var SPEAKING_VOICE = (process.platform == 'darwin' ? 'Alex' : '');
-var PROXIMITY_SENSOR_PIN = 10;
+var dotenv = require('dotenv');
+dotenv.load();
+
+var voice = process.env.VOICE || (process.platform == 'darwin' ? 'Daniel Compact' : '');
+var PROXIMITY_SENSOR_PIN = process.env.PROXIMITY_SENSOR_PIN || 10;
 
 var weatherValidator = validator(6, 9);
 var weatherAnchor = new anchors.Weather({
-    location: "Toronto",
-    apikey: "ZnfDV34F32Qh_qh3mNWg3fxU4m2MyFTDjYvKZwkC7W84iUZTgCjoNmOEum4arrPw",
-    voice: SPEAKING_VOICE,
-    debug: false,
+    location: process.env.WEATHER_LOCATION,
+    apikey: process.env.WEATHER_KEY,
+    debug: process.env.DEBUG,
+    voice: voice
 });
 
 var tvValidator = validator(16, 22);
 var tvAnchor = new anchors.TV({
     url: (process.platform == 'darwin' ? '192.168.0.109:8081' : 'localhost:8081'),
-    apikey: process.argv[2],
-    voice: SPEAKING_VOICE,
-    debug: false
+    apikey: process.env.SICKBEARD_KEY,
+    debug: process.env.DEBUG,
+    voice: voice,
 });
 
 var bouncer = debouncer(function(value) {
@@ -34,10 +37,19 @@ var bouncer = debouncer(function(value) {
     }
 }, 500);
 
-var five = require("johnny-five"),
-    board = new five.Board();
+var now = moment();
 
-board.on("ready", function() {
-    this.pinMode(PROXIMITY_SENSOR_PIN, five.Pin.INPUT);
-    this.digitalRead(PROXIMITY_SENSOR_PIN, bouncer.process);
-});
+// Leaving in the morning, we get 1
+// if (value === 1 && weatherValidator.validate(now)) {
+    weatherAnchor.report();
+// } else if (value === 0 && tvValidator.validate(now)) {
+    // tvAnchor.report();
+// }
+
+// var five = require("johnny-five"),
+//     board = new five.Board();
+
+// board.on("ready", function() {
+//     this.pinMode(PROXIMITY_SENSOR_PIN, five.Pin.INPUT);
+//     this.digitalRead(PROXIMITY_SENSOR_PIN, bouncer.process);
+// });
